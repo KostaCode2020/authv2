4c0509ba9d5397c86b6f5c8230d69730c50a257f
KostaCode2020 <kosta.naumoski@gmail.com>
2023-01-06 18:36:58 +0100

    fixed css for login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
@@ -25,6 +26,7 @@
 
                 <x-input id="lastname" class="block mt-1 w-full" type="text" name="lastname" required autofocus />
             </div>
+
             <!-- Lastname -->
 
             <!-- Email Address -->
